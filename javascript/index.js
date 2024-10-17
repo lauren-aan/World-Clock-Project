078da3c21ff6b11d24bc022f45e1eb9ee1d2dfcb
Lauren Grant <126265030+lauren-aan@users.noreply.github.com>
2024-10-17 22:15:43 +0100

    Update date javascript

--- javascript/index.js
+++ javascript/index.js
@@ -9,52 +9,61 @@
 // let edinburghTimeElement = edinburghElement.querySelector(".time");
 // edinburghTimeElement.innerHTML = "15:41 <small>AM</small>";
 
-// Edinburgh
-let edinburghElement = document.querySelector("#edinburgh");
-let edinburghDateElement = edinburghElement.querySelector(".date");
-edinburghDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
+function updateTime() {
+  // Edinburgh
+  let edinburghTime = moment().tz("Europe/London");
+  let edinburghElement = document.querySelector("#edinburgh");
 
-// let edinburghTime = moment();
-// let edinburghTimeElement = edinburghElement.querySelector(".time");
-// edinburghTimeElement.innerHTML = `${edinburghTime.format(
-//   "h:mm:ss"
-// )}<small>${edinburghTime.format("A")}</small>`;
+  let edinburghDateElement = edinburghElement.querySelector(".date");
+  edinburghDateElement.innerHTML = edinburghTime.format("dddd, MMMM Do YYYY");
 
-let edinburghTime = moment().tz("Europe/London");
-let edinburghTimeElement = edinburghElement.querySelector(".time");
-edinburghTimeElement.innerHTML = edinburghTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  // let edinburghTime = moment();
+  // let edinburghTimeElement = edinburghElement.querySelector(".time");
+  // edinburghTimeElement.innerHTML = `${edinburghTime.format(
+  //   "h:mm:ss"
+  // )}<small>${edinburghTime.format("A")}</small>`;
 
-// Svalbard
-let svalbardElement = document.querySelector("#svalbard");
-let svalbardDateElement = svalbardElement.querySelector(".date");
-svalbardDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
+  let edinburghTimeElement = edinburghElement.querySelector(".time");
+  edinburghTimeElement.innerHTML = edinburghTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-let svalbardTime = moment().tz("Arctic/Longyearbyen");
-let svalbardTimeElement = svalbardElement.querySelector(".time");
-svalbardTimeElement.innerHTML = svalbardTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  // Svalbard
+  let svalbardTime = moment().tz("Arctic/Longyearbyen");
+  let svalbardElement = document.querySelector("#svalbard");
 
-// Auckland
-let aucklandElement = document.querySelector("#auckland");
-let aucklandDateElement = aucklandElement.querySelector(".date");
-aucklandDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
+  let svalbardDateElement = svalbardElement.querySelector(".date");
+  svalbardDateElement.innerHTML = svalbardTime.format("dddd, MMMM Do YYYY");
 
-let aucklandTime = moment().tz("Pacific/Auckland");
-let aucklandTimeElement = aucklandElement.querySelector(".time");
-aucklandTimeElement.innerHTML = aucklandTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  let svalbardTimeElement = svalbardElement.querySelector(".time");
+  svalbardTimeElement.innerHTML = svalbardTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-// Rothera
-let rotheraElement = document.querySelector("#rothera");
-let rotheraDateElement = rotheraElement.querySelector(".date");
-rotheraDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
+  // Auckland
+  let aucklandTime = moment().tz("Pacific/Auckland");
+  let aucklandElement = document.querySelector("#auckland");
 
-let rotheraTime = moment().tz("Antarctica/Rothera");
-let rotheraTimeElement = rotheraElement.querySelector(".time");
-rotheraTimeElement.innerHTML = rotheraTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  let aucklandDateElement = aucklandElement.querySelector(".date");
+  aucklandDateElement.innerHTML = aucklandTime.format("dddd, MMMM Do YYYY");
+
+  let aucklandTimeElement = aucklandElement.querySelector(".time");
+  aucklandTimeElement.innerHTML = aucklandTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Rothera
+  let rotheraTime = moment().tz("Antarctica/Rothera");
+  let rotheraElement = document.querySelector("#rothera");
+
+  let rotheraDateElement = rotheraElement.querySelector(".date");
+  rotheraDateElement.innerHTML = rotheraTime.format("dddd, MMMM Do YYYY");
+
+  let rotheraTimeElement = rotheraElement.querySelector(".time");
+  rotheraTimeElement.innerHTML = rotheraTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
